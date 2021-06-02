8d96e26131063461ea81a80fb63607be50f5cb83
gretaegan <greta.egan@gmail.com>
2021-06-02 14:16:50 +0000

    Fix error in login function

--- app.py
+++ app.py
@@ -54,22 +54,24 @@ def login():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome back, {}".format(
-                        request.form.get("username")))
-                    redirect(url_for(
-                        "dashboard", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
-                flash("Password username or password, please try again!")
+                # invalid password match
+                flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
-    else:
-        flash("Password or username is incorrect. Please try again!")
-        return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
