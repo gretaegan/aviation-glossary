6cc84874e1fd020fb9807ca868ab9dabc869261c
gretaegan <greta.egan@gmail.com>
2021-06-04 10:21:47 +0000

    Add function to delete definitions from database

--- app.py
+++ app.py
@@ -134,6 +134,13 @@ def edit_definition(definition_id):
         "edit_definition.html", definition=definition, categories=categories)
 
 
+@app.route("/delete_definition/<definition_id>")
+def delete_definition(definition_id):
+    mongo.db.definitions.remove({"_id": ObjectId(definition_id)})
+    flash("Definition deleted")
+    return redirect(url_for("get_definitions"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
