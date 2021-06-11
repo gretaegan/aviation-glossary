d7f2066c21297ac9ba9a72e457188dccb596849a
gretaegan <greta.egan@gmail.com>
2021-06-11 12:57:22 +0000

    Create 500 error page and function

--- app.py
+++ app.py
@@ -170,6 +170,11 @@ def not_found(error):
     return render_template('404.html', error=error), 404
 
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
